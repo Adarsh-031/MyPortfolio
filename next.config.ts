f83b4e4492f428496f38ab1ed72b8e8034167006
Adarsha <adarsh.edurkala33@gmail.com>
2026-02-13 20:35:52 +0530

    updates next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    CODEFORCES_HANDLE : process.env.CODEFORCES_HANDLE,
+  },
 };
 
 export default nextConfig;
